ccb5bb8a4076cdcd0ab0459c0b832f44a38d90a2
naliliV2 <nalili0000007@gmail.com>
2021-10-18 17:46:13 +0200

    Release 1.0

---
+++ course aux nombres/Multiplicator/main.py
@@ -0,0 +1,24 @@
+    import matplotlib.pyplot as plt
+
+def main():
+    nb = int(input("Donner un nombre \n>>> "))
+
+    grid = [i for i in range(1, nb+1)]
+
+    for i in grid: #X
+        print(i)
+        for j in grid: #Y
+
+            if i > j:
+                if i % j == 0:
+                    plt.scatter(str(j), str(i), color="red")
+            elif i < j:
+                if j % i == 0:
+                    plt.scatter(str(j), str(i), color="blue")
+            elif i == j:
+                plt.scatter(str(j), str(i), color="white")
+
+    plt.show()
+
+    if __name__ == "__main__":
+        main()
